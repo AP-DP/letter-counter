f4ccc634fe7354260930e641a378c55673b8a2ce
APDP <ap_duplessis@live.com>
2024-03-10 14:36:11 -0700

    New component GridDisplay - set up basic structure.

---
+++ letter-counter/app/components/gridDisplay.js
@@ -0,0 +1,16 @@
+'use client'
+
+// React Elements
+import React from 'react';
+// Bootstrap Elements
+import 'bootstrap/dist/css/bootstrap.min.css';
+// CSS
+import styles from "../styles/grid.module.css";
+
+function GridDisplay() {
+    return (
+        <div></div>
+    );
+}
+
+export default GridDisplay;
