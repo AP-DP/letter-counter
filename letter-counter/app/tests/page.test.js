a17dcef148bd35f5fd4aeb0c2a45a95c7f87c389
APDP <ap_duplessis@live.com>
2024-03-12 20:40:04 -0700

    Updated page tests to reflect new content (includes snapshot test)

--- letter-counter/app/tests/page.test.js
+++ letter-counter/app/tests/page.test.js
@@ -3,11 +3,18 @@
  */
 import {expect, test} from "@jest/globals";
 import { render, screen } from "@testing-library/react";
+import renderer from 'react-test-renderer';
 import Home from "../page";
 
-test("All links present", () => {
+test("Intro renders correctly", () => {
   render(<Home />);
-  const links = screen.getAllByRole("link")
-  const num_links = links.length
-  expect(num_links).toBe(5);
+  const paragraph = screen.getByText((content, element) => content.startsWith('Get started'))
+  expect(paragraph.innerHTML).toBe("Get started by typing into the box below.");
+});
+
+test('Page renders correctly', () => {
+  const tree = renderer
+    .create(<Home></Home>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
